5fca69509feca8c1abc4205635890e950b16fcf5
stefano mazziotta <smazziotta@foodlus.com>
2024-06-30 22:47:13 -0300

    develop insert logic

--- cache.h
+++ cache.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <map>
 #include <utility>
 #include <string>
@@ -10,16 +11,19 @@ template <class T>
 class CacheManager
 {
 private:
-    int capacity;
-    map<string, pair<T, string>> cache_data; // (id, (object, MRU))
+    size_t capacity;
+    map<string, pair<T, int>> cache_data; // (id, (object, MRU))
     bool write_file(string, T);
+    int get_maximum_MRU();
+    bool exists_in_cache(string key);
 
 public:
     CacheManager(int); // recibe la capacidad en el int
     ~CacheManager();
 
     void insert(string id, T object);
-    T get(string id);
+    // T get(string id);
+    int get(string id);
     void show_cache();
 };
 
@@ -29,45 +33,143 @@ CacheManager<T>::CacheManager(int capacity)
     this->capacity = capacity;
 
     // get class name T and create a file with this name
-    std::ofstream file(T::class_name + ".txt");
+    fstream file(T::class_name + ".txt");
 }
 
 template <class T>
 CacheManager<T>::~CacheManager() {}
 
 template <class T>
-bool CacheManager<T>::write_file(std::string id, T object)
+bool CacheManager<T>::write_file(string key, T object)
 {
-    // write object into file
-}
+    string filename = T::class_name + ".txt";
+    string temp_filename = filename + ".tmp";
 
-template <class T>
-void CacheManager<T>::insert(std::string id, T object)
-{
-    // Cada llamada a insert(),
+    ifstream input_file(filename);
+    ofstream temp_file(temp_filename);
 
-    // La inserción del T Object se realizará en la cache y en el archivo.
+    bool key_exists = false;
+    bool write_success = false;
 
-    // Si ya existía la clave, actualiza el Object. (en cache y en el archivo)
-    // De lo contrario, escribirá el objeto en el sistema de archivos y la caché
-    // Se actualizará el MRU del Object para que sea el mayor.
-
-    // Check if the id already exists in the cache
-    if (cache_data.find(id) != cache_data.end())
+    if (!input_file || !temp_file)
     {
-        cache_data[id].first = object; // Id already exists, update the object
+        cerr << "Error opening file." << endl;
+        return false;
+    }
+
+    string line;
+    while (getline(input_file, line))
+    {
+        istringstream iss(line);
+        string file_key;
+        T file_value;
+        if (iss >> file_key)
+        {
+            if (file_key == key)
+            {
+                // Key matches, write updated object instead
+                temp_file << key << " " << object << endl;
+                key_exists = true;
+                write_success = true;
+            }
+            else
+            {
+                // Write the original line to temp file
+                temp_file << line << endl;
+            }
+        }
+    }
+
+    if (!key_exists)
+    {
+        // Key does not exist, append it to the temp file
+        temp_file << key << " " << object << endl;
+        write_success = true;
+    }
+
+    input_file.close();
+    temp_file.close();
+
+    if (write_success)
+    {
+        // Replace the original file with the updated temp file
+        remove(filename.c_str());
+        rename(temp_filename.c_str(), filename.c_str());
     }
     else
     {
-        // id does not exist, insert the object into the cache
-        cache_data[id] = std::make_pair(object, ""); // verify MRU
+        // Cleanup temp file if write was not successful
+        remove(temp_filename.c_str());
     }
 
-    write_file(id, object);
+    return write_success;
 }
 
 template <class T>
-T CacheManager<T>::get(std::string id)
+bool CacheManager<T>::exists_in_cache(string key)
+{
+    // Check if the key already exists in the cache
+    bool exists = cache_data.find(key) != cache_data.end();
+    return exists;
+}
+
+template <class T>
+int CacheManager<T>::get_maximum_MRU()
+{
+    // find the maximum MRU
+    int maximum_MRU = 0;
+    for (const auto &pair : cache_data)
+    {
+        if (pair.second.second > maximum_MRU)
+        {
+            maximum_MRU = pair.second.second;
+        }
+    }
+
+    return maximum_MRU;
+}
+
+template <class T>
+void CacheManager<T>::insert(string key, T object)
+{
+    // Check if the key already exists in the cache
+    if (this->exists_in_cache(key))
+    {
+        cache_data[key].first = object;
+
+        int previous_MRU = cache_data[key].second;
+        cache_data[key].second = previous_MRU + 1;
+
+        write_file(key, object);
+        return;
+    }
+
+    // key doesn't exist
+
+    // if the cache is full, remove the least recently used object
+    if (cache_data.size() >= capacity)
+    {
+        // Find the least recently used object
+        auto it = cache_data.begin();
+        for (auto iterator = cache_data.begin(); iterator != cache_data.end(); iterator++)
+        {
+            if (iterator->second.second < it->second.second)
+            {
+                it = iterator;
+            }
+        }
+        // Remove the least recently used object from the cache
+        cache_data.erase(it);
+    }
+
+    int maximum_MRU = get_maximum_MRU() + 1;
+    cache_data.emplace(key, make_pair(move(object), maximum_MRU)); // insert to cache
+
+    write_file(key, object);
+}
+
+template <class T>
+int CacheManager<T>::get(string key)
 {
     /**
     Al realizar un get(), si no se encuentra en la cache en RAM se lo buscará en
@@ -82,83 +184,84 @@ T CacheManager<T>::get(std::string id)
     */
 
     // Check if the id exists in the cache
-    if (cache_data.find(id) != cache_data.end())
-    {
-        // id found in cache, return the value
-        return cache_data[id].first;
-    }
+    // if (cache_data.find(id) != cache_data.end())
+    // {
+    //     // id found in cache, return the value
+    //     return cache_data[id].first;
+    // }
 
     // id not found in cache, search in the file
-    std::ifstream file("cache.txt");
-    std::string line;
-    while (std::getline(file, line))
-    {
-        // Parse the line to extract the id and value
-        std::istringstream iss(line);
-        std::string fileKey;
-        T fileValue;
-        if (iss >> fileKey >> fileValue)
-        {
-            // Check if the id matches
-            if (fileKey == id)
-            {
-                // Id found in the file, add it to the cache
-                cache_data[id] = std::make_pair(fileValue, "");
-                file.close();
-                return fileValue;
-            }
-        }
-    }
-    file.close();
+    // ifstream file("cache.txt");
+    // string line;
+    // while (std::getline(file, line))
+    // {
+    //     // Parse the line to extract the id and value
+    //     istringstream iss(line);
+    //     string fileKey;
+    //     T fileValue;
+    //     if (iss >> fileKey >> fileValue)
+    //     {
+    //         // Check if the id matches
+    //         if (fileKey == id)
+    //         {
+    //             // Id found in the file, add it to the cache
+    //             cache_data[id] = make_pair(fileValue, "");
+    //             file.close();
+    //             return fileValue;
+    //         }
+    //     }
+    // }
+    // file.close();
 
-    // If the id is found in the file, add it to the cache
-    // TODO: Add the id-objet pair to the cache
-    // Add the id-value pair to the cache
-    cache_data[id] = std::make_pair(object, "MRU");
+    // // If the id is found in the file, add it to the cache
+    // // TODO: Add the id-objet pair to the cache
+    // // Add the id-value pair to the cache
+    // cache_data[id] = std::make_pair(object, "MRU");
 
-    // Implement LRU eviction logic if the cache is full
-    if (cache_data.size() > capacity)
-    {
-        // Find the least recently used id
-        auto it = cache_data.begin();
-        for (auto i = cache_data.begin(); i != cache_data.end(); i++)
-        {
-            if (i->second.second < it->second.second)
-            {
-                it = i;
-            }
-        }
-        // Remove the least recently used id from the cache
-        cache_data.erase(it);
-    }
+    // // Implement LRU eviction logic if the cache is full
+    // if (cache_data.size() > capacity)
+    // {
+    //     // Find the least recently used id
+    //     auto it = cache_data.begin();
+    //     for (auto i = cache_data.begin(); i != cache_data.end(); i++)
+    //     {
+    //         if (i->second.second < it->second.second)
+    //         {
+    //             it = i;
+    //         }
+    //     }
+    //     // Remove the least recently used id from the cache
+    //     cache_data.erase(it);
+    // }
 
-    // TODO: Implement LRU eviction logic if the cache is full
-    // Implement LRU eviction logic if the cache is full
-    if (cache_data.size() > capacity)
-    {
-        // Find the least recently used id
-        auto it = cache_data.begin();
-        for (auto i = cache_data.begin(); i != cache_data.end(); i++)
-        {
-            if (i->second.second < it->second.second)
-            {
-                it = i;
-            }
-        }
-        // Remove the least recently used id from the cache
-        cache_data.erase(it);
-    }
+    // // TODO: Implement LRU eviction logic if the cache is full
+    // // Implement LRU eviction logic if the cache is full
+    // if (cache_data.size() > capacity)
+    // {
+    //     // Find the least recently used id
+    //     auto it = cache_data.begin();
+    //     for (auto i = cache_data.begin(); i != cache_data.end(); i++)
+    //     {
+    //         if (i->second.second < it->second.second)
+    //         {
+    //             it = i;
+    //         }
+    //     }
+    //     // Remove the least recently used id from the cache
+    //     cache_data.erase(it);
+    // }
 
-    // Return the value associated with the id
-    return cache_data[id].first;
+    // // Return the value associated with the id
+    // return cache_data[id].first;
+    return 0;
 }
 
 template <class T>
 void CacheManager<T>::show_cache()
 {
     // Muestra el contenido de la cache
-    for (auto i = cache_data.begin(); i != cache_data.end(); i++)
+    for (auto map_data = cache_data.begin(); map_data != cache_data.end(); map_data++)
     {
-        std::cout << "Id: " << i->first << ", Value: (Student: " << i->second.first.name() << ", Info: " << i->second.second << ")" << std::endl;
+        std::cout << "Key: " << map_data->first << ", Value: (Object: " << map_data->second.first.print() << ", MRU: " << map_data->second.second << ")" << std::endl;
     }
 }
--- main.cpp
+++ main.cpp
@@ -3,21 +3,29 @@
 class Student
 {
     int id;
-    int value;
-    const char *data;
+    const char *name;
+    int age;
 
 public:
     static const std::string class_name; // cada clase tiene un static llamado: class_name
-    Student(int _id, int _value, const char *_data) : id(_id), value(_value), data(_data) {}
+    Student() = default;
+    Student(int _id, const char *_name, int _age) : id(_id), name(_name), age(_age) {}
     void print()
     {
-        cout << "Student Object: " << id << " " << value << " " << data << endl;
+        cout << "Student: " << id << " " << name << " " << age << "years old" << endl;
     }
+
     ~Student() {}
 
     int getId() { return id; }
-    int getValue() { return value; }
-    const char *getData() { return data; }
+    int getAge() { return age; }
+    const char *getName() { return name; }
+
+    friend std::ostream &operator<<(std::ostream &os, const Student &student)
+    {
+        os << student.id << " " << student.name << " " << student.age;
+        return os;
+    }
 };
 
 const string Student::class_name = "StudentClass";
@@ -25,31 +33,29 @@ const string Student::class_name = "StudentClass";
 int main()
 {
     CacheManager<Student> student_cache(3);
-    student_cache.insert("0", Student(0, 22, "student1"));
-    student_cache.insert("1", Student(1, 23, "student2"));
-    student_cache.insert("2", Student(2, 25, "student3"));
-    student_cache.insert("3", Student(3, 29, "student4"));
+    student_cache.insert("1", Student(1, "Pepito", 22));
+    // student_cache.insert("2", Student(2, 23, "María"));
 
-    student_cache.show_cache();
+    // student_cache.show_cache();
 
     cout << "------------------ UPDATE ------------------" << endl;
 
-    student_cache.insert("2", Student(22, 222, "EZE"));
-    student_cache.show_cache();
+    // student_cache.insert("2", Student(22, 222, "EZE"));
+    // student_cache.show_cache();
 
     cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
 
-    Student return_obj = student_cache.get("0"); // not in cache but in filesystem
+    // Student return_obj = student_cache.get("0"); // not in cache but in filesystem
 
     cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
 
-    student_cache.insert("2", Student(2, 25, "EZE"));
-    student_cache.show_cache();
+    // student_cache.insert("2", Student(2, 25, "EZE"));
+    // student_cache.show_cache();
 
-    student_cache.insert("4", Student(4, 29, "student5"));
-    student_cache.insert("5", Student(5, 29, "student6"));
-    student_cache.insert("5", Student(5, 29, "student6"));
-    student_cache.show_cache();
+    // student_cache.insert("4", Student(4, 29, "student5"));
+    // student_cache.insert("5", Student(5, 29, "student6"));
+    // student_cache.insert("5", Student(5, 29, "student6"));
+    // student_cache.show_cache();
 
     return 0;
 }
